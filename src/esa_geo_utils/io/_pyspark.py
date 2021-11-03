30d11e0ee57973d08f312a0da97e808bab29e894
Ed Fawcett-Taylor <ed.fawcett-taylor@defra.gov.uk>
2021-11-03 14:29:48 +0000

    update return statements in _vector_file_to_pdf

--- src/esa_geo_utils/io/_pyspark.py
+++ src/esa_geo_utils/io/_pyspark.py
@@ -187,13 +187,12 @@ def _vector_file_to_pdf(
     feature_names = _get_property_names(layer=_layer) + tuple([geom_field_name])
     pdf = PandasDataFrame(data=features_generator, columns=feature_names)
     if coerce_to_schema:
-        return _coerce_to_schema(
+        pdf = _coerce_to_schema(
             pdf=pdf,
             schema=schema,
             spark_to_pandas_type_map=spark_to_pandas_type_map,
         )
-    else:
-        return pdf
+    return pdf
 
 
 def _get_paths(directory: str, suffix: str) -> Tuple[Any, ...]:
