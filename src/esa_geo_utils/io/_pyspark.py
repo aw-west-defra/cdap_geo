a9e17616e3510af4b50267b19adc19e5306548cd
Ed Fawcett-Taylor <ed.fawcett-taylor@defra.gov.uk>
2021-11-04 10:55:56 +0000

    remove use of SetConfigOption and UseExceptions from _vector_file_to_pdf as it isn't making any difference

--- src/esa_geo_utils/io/_pyspark.py
+++ src/esa_geo_utils/io/_pyspark.py
@@ -5,8 +5,7 @@ from types import MappingProxyType
 from typing import Any, Callable, Dict, Generator, Optional, Tuple, Union
 
 from numpy import bytes0, float32, int32, int64, object0, str0
-from osgeo.gdal import SetConfigOption
-from osgeo.ogr import DataSource, Feature, GetFieldTypeName, Layer, Open, UseExceptions
+from osgeo.ogr import DataSource, Feature, GetFieldTypeName, Layer, Open
 from pandas import DataFrame as PandasDataFrame
 from pandas import Series
 from pyspark.sql import DataFrame as SparkDataFrame
@@ -214,10 +213,6 @@ def _vector_file_to_pdf(
     sql_kwargs: Optional[Dict[str, str]],
 ) -> PandasDataFrame:
     """Given a file path and layer, returns a pandas DataFrame."""
-    UseExceptions()
-
-    SetConfigOption("GML_FIELDTYPES", "ALWAYS_STRING")
-
     data_source = Open(path)
     if data_source is None:
         return _null_data_frame_from_schema(schema=schema)
