d0547930ba4ccd67fae749dbae4b952be72bc78b
Ed Fawcett-Taylor <ed.fawcett-taylor@defra.gov.uk>
2021-12-07 15:27:20 +0000

    revert non-relevant files to develop branch

--- tests/io/conftest.py
+++ tests/io/conftest.py
@@ -1,13 +1,8 @@
 """Module level fixtures."""
-from pathlib import Path
-from types import MappingProxyType
-
 from _pytest.tmpdir import TempPathFactory
 from geopandas import GeoDataFrame
 from pytest import fixture
 from shapely.geometry import Point
-
-from esa_geo_utils.io._pyspark import OGR_TO_SPARK
 
 
 @fixture
@@ -15,8 +10,7 @@ def first_layer() -> GeoDataFrame:
     """First dummy layer."""
     return GeoDataFrame(
         data={
-            "id": [0, 1],
-            "category": ["A", "B"],
+            "id": ["first_id", "second_id"],
             "geometry": [Point(0, 0), Point(1, 0)],
         },
         crs="EPSG:27700",
@@ -28,8 +22,7 @@ def second_layer() -> GeoDataFrame:
     """Second dummy layer."""
     return GeoDataFrame(
         data={
-            "id": [0, 1],
-            "category": ["C", "D"],
+            "id": ["first_id", "second_id"],
             "geometry": [Point(1, 1), Point(0, 1)],
         },
         crs="EPSG:27700",
@@ -37,21 +30,13 @@ def second_layer() -> GeoDataFrame:
 
 
 @fixture
-def directory_path(
-    tmp_path_factory: TempPathFactory,
-) -> Path:
-    """Pytest temporary directory as Path object."""
-    return tmp_path_factory.getbasetemp()
-
-
-@fixture
 def fileGDB_path(
-    directory_path: Path,
+    tmp_path_factory: TempPathFactory,
     first_layer: GeoDataFrame,
     second_layer: GeoDataFrame,
 ) -> str:
     """Writes dummy layers to FileGDB and returns path as string."""
-    path = directory_path / "data_source.gdb"
+    path = tmp_path_factory.getbasetemp() / "data_source.gdb"
 
     path_as_string = str(path)
 
@@ -68,9 +53,3 @@ def fileGDB_path(
     )
 
     return path_as_string
-
-
-@fixture
-def ogr_to_spark_mapping() -> MappingProxyType:
-    """OGR to Spark data type mapping."""
-    return OGR_TO_SPARK
--- tests/io/test__pyspark.py
+++ tests/io/test__pyspark.py
@@ -1,58 +1,34 @@
 """Tests for _pyspark module."""
-from pathlib import Path
-from types import MappingProxyType
-from typing import Optional, Union
-
+from typing import Dict, Optional, Union
 
 import pytest
 from osgeo.ogr import Layer, Open
-from pyspark.sql.types import BinaryType, LongType, StringType, StructField, StructType
-from shapely.geometry import Point
-from shapely.wkb import loads
 
-from esa_geo_utils.io._pyspark import (
-    _create_schema,
-    _get_feature_schema,
-    _get_features,
-    _get_geometry,
-    _get_layer,
-    _get_paths,
-    _get_properties,
-    _get_property_names,
-    _get_property_types,
-)
+from esa_geo_utils.io._pyspark import _get_layer
 
-_get_layer_PARAMETER_NAMES = [
-    "layer",
-    "start",
-    "stop",
-    "expected_layer_name",
-    "expected_feature_count",
-]
-
-_get_layer_PARAMETER_VALUES = [
+PARAMETERS = [
     (None, None, None, "second", 2),
-    (0, None, None, "second", 2),
-    ("first", None, None, "first", 2),
-]
-
-_get_layer_PARAMETER_IDS = [
-    "No arguments",
-    "Layer by index",
-    "Layer by name",
+    (None, None, 0, "second", 2),
+    (None, None, "first", "first", 2),
+    ("SELECT * FROM first WHERE id LIKE 'first_id'", None, None, "first", 1),
 ]
 
 
 @pytest.mark.parametrize(
-    argnames=_get_layer_PARAMETER_NAMES,
-    argvalues=_get_layer_PARAMETER_VALUES,
-    ids=_get_layer_PARAMETER_IDS,
+    "sql,sql_kwargs,layer,expected_layer_name,expected_feature_count",
+    PARAMETERS,
+    ids=[
+        "No arguments",
+        "Layer by index",
+        "Layer by name",
+        "Layer by SQL",
+    ],
 )
 def test__get_layer(
     fileGDB_path: str,
+    sql: Optional[str],
+    sql_kwargs: Optional[Dict[str, str]],
     layer: Optional[Union[str, int]],
-    start: int,
-    stop: int,
     expected_layer_name: str,
     expected_feature_count: int,
 ) -> None:
@@ -61,9 +37,9 @@ def test__get_layer(
 
     _layer: Layer = _get_layer(
         data_source=data_source,
+        sql=sql,
         layer=layer,
-        start=start,
-        stop=stop,
+        sql_kwargs=sql_kwargs,
     )
 
     layer_name = _layer.GetName()
@@ -73,120 +49,3 @@ def test__get_layer(
     assert (layer_name == expected_layer_name) & (
         feature_count == expected_feature_count
     )
-
-
-def test__get_property_names(fileGDB_path: str) -> None:
-    """Returns expected non-geometry field names."""
-    data_source = Open(fileGDB_path)
-    layer = data_source.GetLayer()
-    property_names = _get_property_names(layer=layer)
-    assert property_names == ("id", "category")
-
-
-def test__get_property_types(fileGDB_path: str) -> None:
-    """Returns expected non-geometry field types."""
-    data_source = Open(fileGDB_path)
-    layer = data_source.GetLayer()
-    property_types = _get_property_types(layer=layer)
-    assert property_types == ("Integer64", "String")
-
-
-def test__get_feature_schema(
-    fileGDB_path: str, ogr_to_spark_mapping: MappingProxyType
-) -> None:
-    """Returns expected Spark schema."""
-    data_source = Open(fileGDB_path)
-    layer = data_source.GetLayer()
-    schema = _get_feature_schema(
-        layer=layer,
-        ogr_to_spark_type_map=ogr_to_spark_mapping,
-        geom_field_name="geometry",
-        geom_field_type="Binary",
-    )
-    assert schema == StructType(
-        [
-            StructField("id", LongType()),
-            StructField("category", StringType()),
-            StructField("geometry", BinaryType()),
-        ]
-    )
-
-
-def test__get_paths(directory_path: Path, fileGDB_path: str) -> None:
-    """Returns collection of FileGDB file paths."""
-    paths = _get_paths(directory=str(directory_path), suffix="gdb")
-    assert paths == tuple([fileGDB_path])
-
-
-_create_schema_PARAMETER_NAMES = [
-    "layer",
-]
-
-_create_schema_PARAMETER_VALUES = [
-    (None,),
-    (0,),
-    ("first",),
-]
-
-_create_schema_PARAMETER_IDS = [
-    "No arguments",
-    "Layer by index",
-    "Layer by name",
-]
-
-
-@pytest.mark.parametrize(
-    argnames=_create_schema_PARAMETER_NAMES,
-    argvalues=_create_schema_PARAMETER_VALUES,
-    ids=_create_schema_PARAMETER_IDS,
-)
-def test__create_schema(
-    fileGDB_path: str,
-    ogr_to_spark_mapping: MappingProxyType,
-    layer: Optional[Union[str, int]],
-) -> None:
-    """Returns expected Spark schema regardless of `_get_layer` function used."""
-    schema = _create_schema(
-        paths=tuple([fileGDB_path]),
-        geom_field_name="geometry",
-        geom_field_type="Binary",
-        ogr_to_spark_type_map=ogr_to_spark_mapping,
-        layer=layer,
-    )
-    assert schema == StructType(
-        [
-            StructField("id", LongType()),
-            StructField("category", StringType()),
-            StructField("geometry", BinaryType()),
-        ]
-    )
-
-
-def test__get_properties(fileGDB_path: str) -> None:
-    """Properties from 0th row from 0th layer."""
-    data_source = Open(fileGDB_path)
-    layer = data_source.GetLayer()
-    feature = layer.GetFeature(0)
-    properties = _get_properties(feature)
-    assert properties == (0, "C")
-
-
-def test__get_geometry(fileGDB_path: str) -> None:
-    """Geometry from 0th row from 0th layer."""
-    data_source = Open(fileGDB_path)
-    layer = data_source.GetLayer()
-    feature = layer.GetFeature(0)
-    geometry = _get_geometry(feature)
-    shapely_object = loads(bytes(geometry[0]))
-    assert shapely_object == Point(1, 1)
-
-
-def test__get_features(fileGDB_path: str) -> None:
-    """All fields from the 0th row of the 0th layer."""
-    data_source = Open(fileGDB_path)
-    layer = data_source.GetLayer()
-    features_generator = _get_features(layer)
-    *properties, geometry = next(features_generator)
-    shapely_object = loads(bytes(geometry))
-    assert tuple(properties) == (0, "C")
-    assert shapely_object == Point(1, 1)
