03c945dd037ba91201ac3abec1c5e8ffd77fc293
aw-west-defra <andrew.west@defra.gov.uk>
2022-02-07 19:14:44 +0000

    something wrong with shapely

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
   install_requires = [
     'GDAL >=3',
     'pyspark >=3',
-    'shapely @ git+https://github.com/shapely/shapely.git',
+    # 'shapely @ git+https://github.com/shapely/shapely.git',
     'geopandas >=0.8',
     'esa_geo_utils @ git+https://github.com/Defra-Data-Science-Centre-of-Excellence/esa_geo_utils.git',
   ],
