19f3da9fe0821805e673f0ffcc08b8f7c2e64f41
aw-west-defra <andrew.west@defra.gov.uk>
2022-02-07 16:13:27 +0000

    still learning about setup

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
-from setuptools import setup, find_packages
+from .src import __version__
+from setuptools import setup
 
 setup(
-    name = 'cdap_geo',
-    version = '0.1',
-    description = 'Geospatial functions for CDAP.',
-    long_description = open('README.md').read(),
-    author = 'Andrew West',
-    url = 'https://github.com/aw-west-defra/cdap_geo',
-    license = None,#open('LICENSE').read(),
-    packages = find_packages(exclude=('tests', 'docs'))
+  name = 'cdap_geo',
+  version = __version__,
+  description = 'Geospatial functions for CDAP.',
+  long_description = open('README').read(),
+  author = 'Andrew West',
+  url = 'https://github.com/aw-west-defra/cdap_geo',
+  license = 'Crown copywrite',
+  package_dir = {'':'src'},
 )
