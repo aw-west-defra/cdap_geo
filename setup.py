ee3970e73abf94755a7a04d5aa023fe6121db100
Andrew West <81236667+aw-west-defra@users.noreply.github.com>
2022-02-23 10:23:34 +0000

    reduce requirements

--- setup.py
+++ setup.py
@@ -12,11 +12,9 @@ setup(
   python_requires = '>=3',
   packages = find_packages(),
   install_requires = [
-    'GDAL >=3',
     'pyspark >=3',
-    # 'shapely @ git+https://github.com/shapely/shapely.git',
     'geopandas >=0.8',
-    'esa_geo_utils @ git+https://github.com/Defra-Data-Science-Centre-of-Excellence/esa_geo_utils.git',
+    'pyspark_vector_files',
   ],
   extras_require = {
     'sedona': ['sedona >=1'],
