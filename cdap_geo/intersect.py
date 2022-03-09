fddb682ec31621f03f4d7ca9251c07a704a98186
Andrew West <81236667+aw-west-defra@users.noreply.github.com>
2022-03-09 19:33:32 +0000

    new index_intersect functions, maybe this time?
    Essentially just removed the `groupBy('index_left')`.
    But also the `keep_` options.

--- cdap_geo/intersect.py
+++ cdap_geo/intersect.py
@@ -1,5 +1,5 @@
 from .typing import *
-from .utils import spark, wkb
+from .utils import spark, wkb, wkbs
 from esa_geo_utils.indexing import calculate_bng_index
 from pyspark.sql import functions as F, types as T
 
@@ -22,7 +22,7 @@ def intersects_udf(left, right):
 def intersects_pudf(left: Series, right: Geometry) -> Series:
   @F.pandas_udf(returnType=T.BooleanType())
   def cond(left: Series) -> Series:
-    return GeoSeries.from_wkb(left).intersects(right)
+    return wkbs(left).intersects(right)
   return cond(left)
 
 
@@ -41,20 +41,21 @@ def sedona_intersection(df0, df1):
 
 
 # Geometry UDFs
-def index_apply(column: bytearray, resolution: int):
-  # currying resolution
-  @F.udf(returnType=T.ArrayType(T.StringType()))
-  def _index_apply(data: bytearray):
-    return calculate_bng_index(data, resolution=resolution, how='intersects')
-  return _index_apply(column)
-
 @F.udf(returnType=T.BooleanType())
 def index_intersects_udf(left, right):
   return wkb(left).intersects(wkb(right))
 
+@F.pandas_udf(returnType=T.BooleanType())
+def index_intersects_pudf(left, right):
+  return wkbs(left).intersects(wkbs(right))
+
 @F.udf(returnType=T.BinaryType())
 def index_intersection_udf(left, right):
   return wkb(left).intersection(wkb(right)).wkb
+
+@F.pandas_udf(returnType=T.BinaryType())
+def index_intersection_pudf(left, right):
+  return wkbs(left).intersection(wkbs(right)).wkb
 
 @F.udf(returnType=T.BinaryType())
 def index_unary_union(data):
@@ -66,51 +67,45 @@ def buffer(column, resolution):
     return wkb(data).buffer(resolution).wkb
   return _buffer(column)
 
-
 # Index
-def index_collect(col: set, union: bool):
-  if union and col == 'geometry':
-    row = index_unary_union(F.collect_list(col))
-  else:
-    row = F.first(col)
-  return row.alias(col)
+def index_apply(column, resolution):
+  '''Spatial Indexing
+  currying resolution ∈ (1, 10, 100, 1_000, 10_000, 100_000)
+  '''
+  @F.udf(returnType=T.ArrayType(T.StringType()))
+  def _index_apply(column):
+    return calculate_bng_index(column, resolution=resolution, how='intersects')
+  return _index_apply(column)
 
-def index_fun(left: SparkDataFrame, right: SparkDataFrame, fun: Callable,
-    keep_right: bool = False, keep_indexes: bool = False, resolution: int = 100):
-  # Drop
-  if not keep_right:
-    right = right.select('geometry')
-  # Index
-  right = right.withColumnRenamed('geometry', 'geometry_right')
-  left = left.withColumn('id_left', F.monotonically_increasing_id())
-  right = right.withColumn('id_right', F.monotonically_increasing_id())
-  left = left.withColumn('geometry_indexes', index_apply('geometry', resolution))
-  right = right.withColumn('geometry_index', index_apply('geometry_right', resolution))
-  left_id = left.select('id_left', 'geometry_indexes')
-  right_id = right.select('id_right', 'geometry_index')
-  left_id = left_id.withColumn('geometry_index', F.explode('geometry_indexes'))
-  right_id = right_id.withColumn('geometry_index', F.explode('geometry_index'))
-  lookup = left_id.join(right_id, on='geometry_index', how='inner')
-  lookup = lookup.select('id_left', 'id_right').distinct()
-  # Join
-  sdf = lookup.join(left, on='id_left', how='inner').join(right, on='id_right', how='inner')
-  # Drop
-  sdf = sdf.drop('id_right', 'geometry_index')
-  if not keep_indexes:
-    sdf = sdf.drop('geometry_indexes')
-  # Function
-  sdf = fun(sdf)
-  # Drop
-  if not keep_right:
-    sdf = sdf.groupBy('id_left').agg(*[index_collect(col, False) for col in sdf.columns])
-    sdf = sdf.drop('geometry_right')
-  sdf = sdf.drop('id_left')
+def index_sjoin(left, right, resolution):
+  left = ( left
+    .withColumn('index_left', F.monotonically_increasing_id()) )
+  right = ( right
+    .withColumnRenamed('geometry', 'geometry_right')
+    .withColumn('index_right', F.monotonically_increasing_id()) )
+  left_index = ( left
+    .withColumn('index_spatial', index_apply('geometry', resolution))
+    .withColumn('index_spatial', F.explode('index_spatial'))
+    .select('index_left', 'index_spatial') )
+  right_index = ( right
+    .withColumn('index_spatial', index_apply('geometry_right', resolution))
+    .withColumn('index_spatial', F.explode('index_spatial'))
+    .select('index_right', 'index_spatial') )
+  sdf = ( SparkDataFrame
+    .join(left_index, right_index, on='index_spatial')
+    .drop('index_spatial').distinct()
+    .join(left, on='index_left')
+    .join(right, on='index_right') )
   return sdf
 
-def index_intersects(left: SparkDataFrame, right: SparkDataFrame, **kwargs):
-  fun = lambda sdf:  sdf.filter(index_intersects_udf('geometry', 'geometry_right'))
-  return index_fun(left, right, fun, **kwargs)
+def index_intersects(left, right, resolution):
+  sdf = ( index_sjoin(left, right, resolution)
+    .filter(index_intersects_pudf('geometry', 'geometry_right'))
+    .drop('index_left', 'index_right', 'geometry_right') )
+  return sdf
 
-def index_intersection(left: SparkDataFrame, right: SparkDataFrame, **kwargs):
-  fun = lambda sdf:  sdf.withColumn('geometry', index_intersection_udf('geometry', 'geometry_right'))
-  return index_fun(left, right, fun, **kwargs)
+def index_intersection(left, right, resolution):
+  sdf = ( index_sjoin(left, right, resolution)
+    .withColumn('geometry', index_intersection_pudf('geometry', 'geometry_right'))
+    .drop('index_left', 'index_right', 'geometry_right') )
+  return sdf
