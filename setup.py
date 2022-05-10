707bd2063759d34123e78e385b612e120fc1de40
aw-west-defra <andrew.west@defra.gov.uk>
2022-05-10 13:53:10 +0100

    no version control?

--- setup.py
+++ setup.py
@@ -16,8 +16,8 @@ setup(
     'geopandas >=0.8',
   ],
   extras_require = {
-    'sedona >=1',
-    'pyspark_vector_files =0.1',
-    'bng_indexer @ git+https://github.com/Defra-Data-Science-Centre-of-Excellence/bng-indexer',
+    'sedona',
+    'pyspark_vector_files',
+    'git+https://github.com/Defra-Data-Science-Centre-of-Excellence/bng-indexer.git',
   },
 )
