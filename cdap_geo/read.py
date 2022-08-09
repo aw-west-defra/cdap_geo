1c57257810576457699c5eb6a1ca3f050faab2a5
Andrew West <81236667+aw-west-defra@users.noreply.github.com>
2022-08-09 17:36:33 +0100

    dbfs api check

--- cdap_geo/read.py
+++ cdap_geo/read.py
@@ -72,6 +72,8 @@ def unpack_gpb_header(byte_array: bytearray) -> T.StructType:
 
 
 def _read_gpkg(filepath, layer):
+  if filepath.startswith('/dbfs/'):
+    filepath = filepath.replace('/dbfs/', 'dbfs:/')
   sdf = spark.read \
     .format('jdbc') \
     .option('url', f'jdbc:sqlite:{filepath}') \
