29f69d9e9141964298f12e2f1d31c06273150800
Andrew West <81236667+aw-west-defra@users.noreply.github.com>
2022-11-09 14:54:58 +0000

    Update index_geohash.py

--- cdap_geo/index_geohash.py
+++ cdap_geo/index_geohash.py
@@ -1,4 +1,4 @@
-import geohash2 as geohash
+from geohash2 import encode
 
 
 def overlap_gen(str0, str1, /,*):
@@ -16,13 +16,13 @@ def encode_box(xmin, ymin, xmax, ymax, /,*, invert=True):
   if invert:  # For shapely bounds
     xmin, ymin, xmax, ymax = ymin, xmin, ymax, xmax    
   return overlap(
-    geohash.encode(xmin, ymin),
-    geohash.encode(xmax, ymax),
+    encode(xmin, ymin),
+    encode(xmax, ymax),
   )
 
 
-def gdf_geohash(df, /,*, inplace=False):
+def gdf_geohash(df, /,*, column='geohash', inplace=False):
   if not inplace:
     df = df.copy()
-  df['geohash'] = df.apply(lambda df: encode_box(*df.geometry.bounds), axis=1)
+  df[column] = df.apply(lambda df: encode_box(*df.geometry.bounds), axis=1)
   return df
