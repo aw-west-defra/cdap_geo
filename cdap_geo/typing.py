e638e49f6ea5582bb4df51eb6104d3c49d158ad4
Andrew West <81236667+aw-west-defra@users.noreply.github.com>
2023-01-11 12:24:25 +0000

    spark series

--- cdap_geo/typing.py
+++ cdap_geo/typing.py
@@ -1,7 +1,10 @@
 from typing import Union, Callable
 from pyspark.sql.dataframe import DataFrame as SparkDataFrame
+from pyspark.sql.column import Column as SparkSeries
 from pandas import DataFrame as PandasDataFrame, Series
 from geopandas import GeoDataFrame, GeoSeries
 from shapely.geometry.base import BaseGeometry
+
 DataFrame = Union[SparkDataFrame, PandasDataFrame, GeoDataFrame]
+Series = Union[SparkSeries, Series, GeoSeries]
 Geometry = Union[GeoDataFrame, GeoSeries, BaseGeometry]
